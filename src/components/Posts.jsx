621428b3e95d1d6d077322890c9f1a121c5be15b
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2022-12-15 01:51:22 +0300

    pulled data from firebase database

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,10 +1,30 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { getDatabase, onValue, ref } from "firebase/database"
 import app from "../authentication/firebase"
+import { useSelector, useDispatch } from "react-redux"
+import { getPosts } from "../redux/features/postsSlice"
+import { useEffect, useState } from "react"
 
 const Posts = () => {
+    const dispatch = useDispatch();
+    const { posts } = useSelector((state) => state.postsSlice)
+    console.log(posts);
+    useEffect(()=>{
+        const database = getDatabase(app);
+        const postsRef = ref(database, "/posts")
+        onValue(postsRef, (snapshot) =>{
+            const data = snapshot.val()
+            const postsArray = []
+
+            for(let id in data){
+                postsArray.push({id, ...data[id]})
+            }
+            dispatch(getPosts({posts:postsArray}))
+        })
+    },[])
   return (
     <>
-    
+    <div>POSTS</div>
     </>
   )
 }
